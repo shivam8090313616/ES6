efa8d39bbbfd8f466141a49f3dffd50ffcd56b9c
shivam8090313616 <shivam.logelite@gmail.com>
2024-04-19 17:22:33 +0530

    promises

--- apna-college/callback-hell4.js
+++ apna-college/callback-hell4.js
@@ -1,12 +1,15 @@
 function getData(dataId, getNextData){
+    console.log("getting data ...")    
+
     setTimeout(()=>{
         console.log("data",dataId);
         if(getNextData){
         getNextData();
-            
         }
     },4000);
 }
 getData(1, ()=>{
-    getData(2);
+    getData(2, ()=>{
+        getData(3);
+    });
 })
